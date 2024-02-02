78e27914ed6bdf6e17a8128a42bc1ca3fe8cd031
Dylan WO (Oneberry) <dylan.wo@oneberry.com>
2024-02-02 18:24:23 +0800

    test

--- app/components/HomeHero/HomeHero.jsx
+++ app/components/HomeHero/HomeHero.jsx
@@ -7,16 +7,14 @@ const HomeHero = () => {
       <div
         className="hero min-h-screen"
         style={{
-          backgroundImage: "/SingHealthHomeHero.jpeg",
+          backgroundImage: "url(/SingHealthHomeHero.jpeg)",
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Greetings!</h1>
-            <p className="mb-5">
-              Welcome to SingHealth's very own RedBox Kiosk.
-            </p>
+            <p className="mb-5">Welcome to SingHealth RedBox Kiosk.</p>
             <p className="mb-5">
               Click on the button below to quickly jump to our AI Chatbot Page
               now!
